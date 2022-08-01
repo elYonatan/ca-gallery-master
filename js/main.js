6ad127c52d8e068b5c75e028b999a02268267615
unknown <xElbazx@gmail.com>
2022-08-01 15:30:51 +0300

    finished adding dynamic portfolio part

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
-console.log('Starting up');
+
+$(init)
+
+
+
+function init() {
+    renderPortfolio()
+
+}
+
+function renderPortfolio() {
+    const $elPortfolio = $('.portfolio-items-container')
+    const projects = getProjects()
+    //html for the photo themselves
+    const strHTMLs = projects.map(project => `
+    <div class="col-md-4 col-sm-6 portfolio-item">
+    <a class="portfolio-link" data-toggle="modal" onclick="onClickImg('${project.name}')">
+    <div class="portfolio-hover">
+    <div class="portfolio-hover-content">
+    <i class="fa fa-plus fa-3x"><br>${project.title}</i>
+    </div>
+    </div>
+    <img class="img-fluid" src="${project.url}" alt="">
+    </a>
+    <div class="portfolio-caption">
+    <h4>${project.name}</h4>
+    <p class="text-muted">${project.labels[0]}</p>
+    </div>
+    </div>`)
+    $elPortfolio.html(strHTMLs)
+}
+
+function onClickImg(name) {
+    //MODAL
+    console.log('here')
+    const project = selectProject(name)
+    //DOM
+    renderModal(project)
+}
+
+function renderModal(project) {
+    // console.log($('#portfolioModal0'))
+    $('.proj-name').text(project.name)
+    $('.proj-sub-title').text(project.title)
+    $('.proj-desc').text(project.desc)
+    $('.proj-date').text(project.date)
+    $('.proj-client').text(project.client)
+    $('.proj-category').text(project.labels.join(' '))
+    $('.proj-img').attr('src', project.url)
+    $('.proj-link').attr('href', project.link)
+}
+
---
+++ js/services/proj.services.js
@@ -0,0 +1,45 @@
+'use strict'
+
+
+var gProjects
+_createProjects()
+
+
+function getProjects() {
+    console.log(gProjects)
+    return gProjects
+}
+
+function selectProject(name) {
+    return gProjects.find(project => project.name === name)
+}
+
+
+function _createProjects() {
+    gProjects = [_createProject("MineSweeper", "Board game",
+        `This is a project of a game called Minesweeper
+        in which u need to logically think where the mines
+        are located on the board
+        and evade them as much as possible`, new Date(), 'Coding Academy', ['matrixes', 'events'],
+        'https://elyonatan.github.io/Sprint1CA/'),
+        _createProject("MineSweeper", "Board game",
+        `This is a project of a game called Minesweeper
+        in which u need to logically think where the mines
+        are located on the board
+        and evade them as much as possible`, new Date(), 'Coding Academy', ['matrixes', 'events'],
+        'https://elyonatan.github.io/Sprint1CA/')
+    ]
+}
+
+function _createProject(name, title, desc, publishedAt, client, labels, link) {
+    return {
+        name,
+        title,
+        desc,
+        url: `/img/portfolio/${name.toLowerCase()}.jpg`,
+        publishedAt,
+        client,
+        labels,
+        link
+    }
+}
